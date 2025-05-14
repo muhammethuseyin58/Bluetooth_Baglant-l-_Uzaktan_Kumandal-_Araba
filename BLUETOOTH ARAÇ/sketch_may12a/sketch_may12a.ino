618bb428c8e422b764edcc5f56f7f4b60da816c1
Muhammet Hüseyin Yağmur <myagmur-58@hotmail.com>
2025-05-14 19:44:01 +0300

    Mini düzenleme

--- BLUETOOTH ARAÇ/sketch_may12a/sketch_may12a.ino
+++ BLUETOOTH ARAÇ/sketch_may12a/sketch_may12a.ino
@@ -1,43 +1,43 @@
 // Motor pinleri
-int motor1_IN1 = 3;  // Sol motor ileri
-int motor1_IN2 = 4;  // Sol motor geri
-int motor2_IN3 = 5;  // Sağ motor ileri
-int motor2_IN4 = 6;  // Sağ motor geri
+int motor1_IN1 = 3;  
+int motor1_IN2 = 4;  
+int motor2_IN3 = 5;  
+int motor2_IN4 = 6;  
 int emA=9;
 int emB=10;
-char gelenVeri;  // Bluetooth'tan gelen karakter
+char gelenVeri;  
 
 void setup() {
-  // Motor pinleri çıkış olarak ayarlanıyor
+ 
   pinMode(motor1_IN1, OUTPUT);
   pinMode(motor1_IN2, OUTPUT);
   pinMode(motor2_IN3, OUTPUT);
   pinMode(motor2_IN4, OUTPUT);
   pinMode(emA, OUTPUT);
   pinMode(emB, OUTPUT);
-  // Seri haberleşme başlatılıyor (Bluetooth ile)
+  
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     gelenVeri = Serial.read();
-    Serial.println(gelenVeri);  // Debug için
+    Serial.println(gelenVeri);  
 
     switch (gelenVeri) {
-      case 'F':  // İleri
+      case 'F': 
         ileriGit();
         break;
-      case 'B':  // Geri
+      case 'B':  
         geriGit();
         break;
-      case 'L':  // Sola dön
+      case 'L': 
         solaDon();
         break;
-      case 'R':  // Sağa dön
+      case 'R':  
         sagaDon();
         break;
-      case 'y':  // Dur
+      case 'y': 
         dur();
         break;
     }
